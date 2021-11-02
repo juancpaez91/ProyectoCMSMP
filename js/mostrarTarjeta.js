9bae71e9a5bd4232ceb039e35490c54900691b8c
belenduhart <mbelenduhart@gmail.com>
2021-11-02 20:56:38 -0300

    tazas

---
+++ js/mostrarTarjeta.js
@@ -0,0 +1,10 @@
+let tazaElegida;
+if (localStorage.getItem("tazaComprar")){
+        tazaElegida = JSON.parse(localStorage.getItem("tazaComprar"))
+}
+
+
+document.querySelector("#imagenTaza").src = tazaElegida.picture ;
+document.querySelector("#nombreTaza").innerHTML = tazaElegida.Nombre;
+document.querySelector("#precioTaza").innerHTML = tazaElegida.Precio;
+document.querySelector("#pagar").href= tazaElegida.boton;
--- js/tarjetaTaza.js
+++ js/tarjetaTaza.js
@@ -1,53 +1,49 @@
-const prueba = {
-    Id:"T001" , Nombre:"Taza Valiente", Precio: 12000, boton:"http://mercadopago.prueba", picture:"../images/Taza-Corazones.jpg"
-}
-
-function ObtenerTaza (){
-    let taza = document.querySelectorAll(".clickFotos");
-    let tazaSeleccionada = taza.target.getAttribute(value);
-    let objetoTaza = Tazas.map( e => e.Id == tazaSeleccionada);
-    console.log(objetoTaza);
-}
-document.querySelector("#imagenTaza").src = prueba.picture ;
-document.querySelector("#nombreTaza").innerHTML = prueba.Nombre;
-document.querySelector("#precioTaza").innerHTML = prueba.Precio;
-document.querySelector("#pagar").href= prueba.boton;
-
-
-
 //Array con tazas
 const Tazas = [
     {
-        Id:"T001" , Nombre:"Taza Valiente", Precio: 12000, Stock:10, picture:"../images/Taza-Corazones.jpg"
+        Id:"T001" , Nombre:"Taza Valiente", Precio: 12000, Stock:10, picture:"https://res.cloudinary.com/sapphiredev/image/upload/v1635102991/CMSMP/tazas/Taza-Valiente_wzivlj.jpg"
     },
     {
-        Id:"T002" , Nombre:"Taza Transformación", Precio: 12000, Stock:10, picture:""
+        Id:"T002" , Nombre:"Taza Transformación", Precio: 12000, Stock:10, picture:"https://res.cloudinary.com/sapphiredev/image/upload/v1635102991/CMSMP/tazas/Taza-Transformacion_av4fz0.jpg"
     },
     {
-        Id:"T003" , Nombre:"Taza Sueña", Precio: 12000, Stock:10, picture:""
+        Id:"T003" , Nombre:"Taza Sueña", Precio: 12000, Stock:10, picture:"https://res.cloudinary.com/sapphiredev/image/upload/v1635102991/CMSMP/tazas/Taza-Sue%C3%B1a_b5wysq.jpg"
     },
     {
-        Id:"T004" , Nombre:"Taza Somos Así", Precio: 12000, Stock:10, picture:""
+        Id:"T004" , Nombre:"Taza Somos Así", Precio: 12000, Stock:10, picture:"https://res.cloudinary.com/sapphiredev/image/upload/v1635102991/CMSMP/tazas/Taza-Somos-Asi_js5n6f.jpg"
     },
     {
-        Id:"T005" , Nombre:"Taza Piensa en Ti", Precio: 12000, Stock:10, picture:""
+        Id:"T005" , Nombre:"Taza Piensa en Ti", Precio: 12000, Stock:10, picture:"https://res.cloudinary.com/sapphiredev/image/upload/v1635102991/CMSMP/tazas/Taza-Piensa-en-Ti_ystkf1.jpg"
     },
     {
-        Id:"T006" , Nombre:"Taza Mujer", Precio: 12000, Stock:10, picture:""
+        Id:"T006" , Nombre:"Taza Mujer", Precio: 12000, Stock:10, picture:"https://res.cloudinary.com/sapphiredev/image/upload/v1635102990/CMSMP/tazas/Taza-Mujer_xj3r36.jpg"
     },
     {
-        Id:"T007" , Nombre:"Taza Mujer Divina", Precio: 12000, Stock:10, picture:""
+        Id:"T007" , Nombre:"Taza Mujer Divina", Precio: 12000, Stock:10, picture:"https://res.cloudinary.com/sapphiredev/image/upload/v1635102990/CMSMP/tazas/Taza-Mujer-Divina_hdgqvr.jpg"
     },
     {
-        Id:"T008" , Nombre:"Taza Maravillosa", Precio: 12000, Stock:10, picture:""
+        Id:"T008" , Nombre:"Taza Maravillosa", Precio: 12000, Stock:10, picture:"https://res.cloudinary.com/sapphiredev/image/upload/v1635102990/CMSMP/tazas/Taza-Maravillosa_z2gwcp.jpg"
     },
     {
-        Id:"T009" , Nombre:"Taza Lo Podemos Lograr", Precio: 12000, Stock:10, picture:""
+        Id:"T009" , Nombre:"Taza Lo Podemos Lograr", Precio: 12000, Stock:10, picture:"https://res.cloudinary.com/sapphiredev/image/upload/v1635102990/CMSMP/tazas/Taza-Lo-Podemos-Lograr_p2qiix.jpg"
     },
     {
-        Id:"T010" , Nombre:"Taza Empoderamiento", Precio: 12000, Stock:10, picture:""
+        Id:"T010" , Nombre:"Taza Empoderamiento", Precio: 12000, Stock:10, picture:"https://res.cloudinary.com/sapphiredev/image/upload/v1635102990/CMSMP/tazas/Taza-Empoderamiento_rpw4ff.jpg"
     },
     {
-        Id:"T011" , Nombre:"Taza Corazones", Precio: 12000, Stock:10, picture:""
+        Id:"T011" , Nombre:"Taza Corazones", Precio: 12000, Stock:10, picture:"https://res.cloudinary.com/sapphiredev/image/upload/v1635102990/CMSMP/tazas/Taza-Corazones_phtfb5.jpg"
     }
 ];
+
+let tazaComprar;
+function ObtenerTaza (clickeado){
+    localStorage.clear();
+    let objetoTaza = Tazas.find( e => e.Id == clickeado);
+    tazaComprar = objetoTaza;
+    localStorage.setItem("tazaComprar", JSON.stringify(tazaComprar));
+    console.log(tazaComprar);
+    location.href = "tazas.html";
+}
+
+
+
