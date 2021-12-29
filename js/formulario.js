bd3585e2252e39b3f4bca98ddcc018fdf05ae55b
belenduhart <mbelenduhart@gmail.com>
2021-12-29 09:16:51 -0300

    updated changes

--- js/formulario.js
+++ js/formulario.js
@@ -25,7 +25,6 @@ function enviarNotificacion(event) {
 
     connect = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
 
-     
     connect.onreadystatechange = function() {
         if (connect.readyState == 4 && connect.status == 200) {
             //console.log("response: " + connect.responseText);
--- js/mostrarTarjeta.js
+++ js/mostrarTarjeta.js
@@ -3,7 +3,6 @@ if (localStorage.getItem("tazaComprar")){
         tazaElegida = JSON.parse(localStorage.getItem("tazaComprar"))
 }
 
-
 document.querySelector("#imagenTaza").src = tazaElegida.picture ;
 document.querySelector("#nombreTaza").innerHTML = tazaElegida.Nombre;
 document.querySelector("#precioTaza").innerHTML = tazaElegida.Precio;
